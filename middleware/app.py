9c83f54eb65df476a4d074b7a89e18a45884cf04
Orel Zabriko <orelzavriko@gmail.com>
2026-07-23 11:25:06 +0300

    fix middleware app.py

--- middleware/app.py
+++ middleware/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from middleware.routes.proxy_routes import proxy_bp
+from services.shared.error_handlers import register_error_handlers
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(proxy_bp)
+    register_error_handlers(app)
     return app
 
 
